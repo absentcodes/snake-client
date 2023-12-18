ebf5de0763813f024a5b5c41f0b3825d2d8e9341
absentcodes <absentpassengercodes@gmail.com>
2023-12-18 23:43:51 +0300

    implemeted event handler for connect event as well as name

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 const net = require("net");
 
 // establishes a connection with the game server
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: '172.20.10.5',// IP address here,
     port: '50541' // PORT number here,
@@ -10,9 +10,19 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
-   //event handler for 'data' event
-   conn.on("data" , (data) => {
-    console.log("Server says:", data)
+
+  //event handler for connect event
+  conn.on("connect", () => {
+    console.log("Successfully connected to the game server!");
+    conn.write("Name: FMF");
+  });
+
+    
+
+
+  //event handler for 'data' event
+  conn.on("data" , (data) => {
+    console.log("Server says:", data);
   });
 
   return conn;
