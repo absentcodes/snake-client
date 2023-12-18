1999465e1c3fb57da478d3b20baf256a278de9e6
absentcodes <absentpassengercodes@gmail.com>
2023-12-18 23:32:07 +0300

    imported connect function from client flie into play file using module exports ES6

--- play.js
+++ play.js
@@ -1,24 +1,6 @@
-const net = require("net");
+//import the connect funtion from client
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '172.20.10.5',// IP address here,
-    port: '50541' // PORT number here,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-   //event handler for 'data' event
-   conn.on("data" , (data) => {
-    console.log("Server says:", data)
-  });
-
-  return conn;
-};
-
- 
+const { connect } = require('./client');
 
 console.log("Connecting ...");
 connect();
